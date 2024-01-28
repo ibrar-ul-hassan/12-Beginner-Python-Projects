a20b420dbb844657ddb03d2ac035f1f2c0bda965
ibrar-ul-hassan <ibrarulhassan967@gmail.com>
2024-01-28 06:48:39 +0500

    Adding the first time comment

---
+++ 12 Python Beginner Projects.py
@@ -0,0 +1,14 @@
+# 1. Madlibs
+# 2. Guess the number (Computer)
+# 3. guess the number (User)
+# 4. Rock Paper Scissors
+# 5. Hangman
+# 6. Tic-Tac-Toe
+# 7. Tic-Tac-Toe- AI
+# 8. Binary Search
+# 9. Minesweeper
+# 10. Sudoku Solver
+# 11. Photo Processing
+# 12. markov Chain Composer
+
+
